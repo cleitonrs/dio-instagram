75c85eff088f96ea8ba7720d0d03141f5d62f270
cleitonrs <rscleitonrs@gmail.com>
2022-05-11 05:50:23 -0300

    fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let images = ["./img/im1.png", "./img/im2.png", "./img/im3.png", "./img/im4.png"]
+let images = ["./img/im2.png", "./img/im3.png", "./img/im4.png", "./img/im1.png"]
 
 let index = 0 
 
@@ -12,3 +12,23 @@ function change() {
 window.onload = function () {
   setInterval(change, 4000)
 }
+
+// Button login enabled/disabled
+
+let user = document.querySelector("#user")
+let password = document.querySelector("#password")
+let login = document.querySelector("#login")
+
+login.disabled = true 
+
+user.addEventListener("change", enabled)
+password.addEventListener("change", enabled)
+
+function enabled() {
+  if (user.value === "" || password.value === "") {
+    login.disabled = true
+  } else {
+    login.disabled = false
+  }
+}
+
